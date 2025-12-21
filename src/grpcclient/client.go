9e3703c0caf5f084ee4809dc7051397b458829f8
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-12-21 20:49:17 +0100

    init

---
+++ src/grpcclient/client.go
@@ -0,0 +1,19 @@
+package grpcclient
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewGRPCClient(addr string) *grpc.ClientConn {
+	conn, err := grpc.Dial(
+		addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalf("failed to connect to grpc server: %v", err)
+	}
+	return conn
+}
